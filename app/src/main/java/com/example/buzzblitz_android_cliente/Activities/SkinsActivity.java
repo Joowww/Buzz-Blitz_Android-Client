73095d1d7577e050ff00026431fbcf114515dc1d
Joowww <joelmoreno2000@gmail.com>
2025-05-11 16:29:43 +0200

    Sigue el error de importar imagenes pero he mejorado y añadido cosas del user

---
+++ app/src/main/java/com/example/buzzblitz_android_cliente/Activities/SkinsActivity.java
@@ -0,0 +1,14 @@
+package com.example.buzzblitz_android_cliente.Activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.buzzblitz_android_cliente.R;
+
+public class SkinsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userskins);
+    }
+}
--- app/src/main/java/com/example/buzzblitz_android_cliente/Activities/UserProfileActivity.java
+++ app/src/main/java/com/example/buzzblitz_android_cliente/Activities/UserProfileActivity.java
@@ -3,12 +3,14 @@ package com.example.buzzblitz_android_cliente.Activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.buzzblitz_android_cliente.R;
 
 public class UserProfileActivity extends BaseActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +22,15 @@ public class UserProfileActivity extends BaseActivity {
         TextView tvName = findViewById(R.id.textView5);
         tvName.setText("Name: " + userName);
 
-
         TextView tvUserIdCorner = findViewById(R.id.tvUserIdCorner);
         tvUserIdCorner.setText(sharedPreferences.getString("currentUserId", ""));
+
+        Button btnInventory = findViewById(R.id.btnGoToInventory);
+
+        btnInventory.setOnClickListener(v ->
+                startActivity(new Intent(UserProfileActivity.this, InventoryActivity.class))
+        );
+
+
     }
 }
